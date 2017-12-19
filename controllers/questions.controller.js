1535d2cea3fed56cd48072894e129390de4d2ef2
harshkhandelwalcs <hkcs1995@gmail.com>
2017-12-19 11:57:19 +0530

    Set questions and amswers

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -1,9 +1,50 @@
 var data = {
-    getQuestions: function(req,res, next) {
+    getQuestions: function (req, res, next) {
         let questionJson = {
-            name: 'Harsh khandelwal'
+            practiceTest1: {
+                q1: {
+                    text: "what is your name?",
+                    options: ['Harsh', 'Pawan', 'Pritam', 'Kapil']
+                },
+                q2: {
+                    text: "what is your hobby?",
+                    options: ['Singing', 'Drawing', 'Playing Cricket', 'Watching cartoon']
+                }
+            },
+            practiceTest2: {
+                q1: {
+                    text: "what is your Age?",
+                    options: ['20', '22', '24', '26']
+                },
+                q2: {
+                    text: "what is your Father's name?",
+                    options: ['S.S.Gupta', 'Kamal', 'S.R.Gupta', 'Kanahiya']
+                }
+            }
         };
         res.send(questionJson);
+        return;
+    },
+    getAnswers: function (req, res, next) {
+        let answerJson = {
+            practiceTest1: {
+                q1: {
+                    ans: "Harsh"
+                },
+                q2: {
+                    ans: "Singing"
+                }
+            },
+            practiceTest2: {
+                q1: {
+                    ans: "22"
+                },
+                q2: {
+                    ans: "S.S.Gupta"
+                }
+            }
+        };
+        res.send(answerJson);
         return;
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ var data = require('../controllers/questions.controller');
 
 /* GET home page. */
 router.get('/', data.getQuestions);
+router.get('/answers',data.getAnswers);
+// router.post('/',)
 
 module.exports = router;
